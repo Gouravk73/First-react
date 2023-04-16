4d540dbb0e3ae7f012a074952a44ea9228c25ba8
Gouravk73 <71483899+Gouravk73@users.noreply.github.com>
2023-04-16 07:00:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import ExpenseItem from "./components/ExpenseItem";
+function App() {
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <ExpenseItem></ExpenseItem>
+      <div>
+        <h4>Food Rs 10</h4>
+        <h4>Petrol Rs 100</h4>
+        <h4>Movies Rs 200</h4>
+      </div>
+    </div>
+    
+  );
+}
+
+export default App;
